e6ac7a158b5fc675ad028151eaa461c9101c1b48
Zach Rider <zrider99zr@gmail.com>
2017-11-23 00:41:13 -0500

    gags

--- api/DBget.php
+++ api/DBget.php
@@ -14,10 +14,9 @@ echo $email;
 $qry = $db->prepare("SELECT salt, hash FROM account WHERE emailAddress = ?");
 $qry->bind_param("s",$email);
 $qry->execute();
-$qry->bind_result($dbSalt,$dbHash);
-$qry->store_result();
+$result = $qry->get_result();
 
-echo "Salt ", $dbSalt, "<br /> Hash " $dbHash;
+echo "Salt ", $result[0]['salt'], "<br /> Hash ", $result[0]['hash'];
 
 
  ?>
