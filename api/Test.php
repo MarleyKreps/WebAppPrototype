c92bb44c9da1ad7357b154932998fab74e5ece65
Zach Rider <zrider99zr@gmail.com>
2017-11-22 21:22:13 -0500

    G

---
+++ api/Test.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+First Name: <?php echo $_GET["firstName"]; ?><br>
+Last Name: <?php echo $_GET["lastName"]; ?><br>
+Email: <?php echo $_GET["email"]; ?><br>
+Password: <?php echo $_GET["password"]; ?><br>
+isClient: <?php echo $_GET["isClient"]; ?><br>
+isAdmin: <?php echo $_GET["isAdmin"]; ?><br>
+</body>
+</html>
