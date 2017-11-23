0be271c55a2127ee21fa7713e5d9c862257c29b5
Zach Rider <zrider99zr@gmail.com>
2017-11-23 00:45:18 -0500

    gasdgs

--- api/DBget.php
+++ api/DBget.php
@@ -11,8 +11,7 @@ $email = "zrider99zr@gmail.com";
 
 echo $email;
 
-$qry = $db->prepare("SELECT salt, hash FROM account WHERE emailAddress = ?");
-$qry->bind_param("s",$email);
+$qry = $db->prepare("SELECT salt, hash FROM account WHERE accountID = 5");
 $qry->execute();
 $rowcnt = $qry->num_rows;
 $result = $qry->get_result();
