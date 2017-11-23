a1723934382ac666759145bc07275a3cf26b2893
Zach Rider <zrider99zr@gmail.com>
2017-11-22 21:24:51 -0500

    Gagds

--- api/Test.php
+++ api/Test.php
@@ -1,11 +1,11 @@
 <html>
 <body>
 
-First Name: <?php echo $_GET["firstName"]; ?><br>
-Last Name: <?php echo $_GET["lastName"]; ?><br>
-Email: <?php echo $_GET["email"]; ?><br>
-Password: <?php echo $_GET["password"]; ?><br>
-isClient: <?php echo $_GET["isClient"]; ?><br>
-isAdmin: <?php echo $_GET["isAdmin"]; ?><br>
+First Name: <?php echo $_POST["firstName"]; ?><br>
+Last Name: <?php echo $_POST["lastName"]; ?><br>
+Email: <?php echo $_POST["email"]; ?><br>
+Password: <?php echo $_POST["password"]; ?><br>
+isClient: <?php echo $_POST["isClient"]; ?><br>
+isAdmin: <?php echo $_POST["isAdmin"]; ?><br>
 </body>
 </html>
