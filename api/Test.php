4c2e92a4caf21db2749407ce1f030fd9a48f4c89
Zach Rider <zrider99zr@gmail.com>
2017-11-22 23:37:03 -0500

    aa

--- api/Test.php
+++ api/Test.php
@@ -1,13 +1,13 @@
 <?php
-/*
+
 require_once(__DIR__ . '/config/global.php');
 function __autoload($className){
   require_once(__DIR__ . '/classes/' . $className . '.php');
 }
-*/
 
 
-//$db = Database::getConnection();
+
+$db = Database::getConnection();
 
 foreach ($_POST as $key => $value) {
   $$key = trim($value);
