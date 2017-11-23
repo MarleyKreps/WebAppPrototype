e71e249dc4e3038554c82f7408d18e3eff35eeb2
Zach Rider <zrider99zr@gmail.com>
2017-11-22 22:59:54 -0500

    Added some stuff

--- api/Test.php
+++ api/Test.php
@@ -4,12 +4,30 @@ function __autoload($className){
   require_once(__DIR__ . '/classes/' . $className . '.php');
 }
 
+
+
 $db = Database::getConnection();
 $test = new Test($dbc);
 
 foreach ($_POST as $key => $value) {
   $$key = trim($val);
-  echo $val;
 }
+
+$salt = random_bytes(32); //create salt for account
+$saltedPassword = $salt.$password;
+$hash = hash('scrypt',$saltedPassword);
+$qry = $this->db->prepare("INSERT INTO account(emailAddress,firstName,lastName,hash,salt) VALUES(?,?,?)");
+$qry->bind_param("sssss",$email,$firstName,$lastName,$hash,$salt);  $qry->execute();
+//Get the institutionID for the insert into clientAccount
+$incrementID = $qry->insert_id;
+//If the created account is a client account create a client account
+
+$this-registerSystemAccount($incrementID);
+$qry->close();
+$qry = $this->db->prepare("INSERT INTO systemAdmin VALUES(?)");
+$qry->bind_param("iii",$accountID);
+$qry->execute();
+$qry->close();
+
 
  ?>
