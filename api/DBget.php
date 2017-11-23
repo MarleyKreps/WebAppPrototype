29812d98d6b00ae514562ae0301584c4bfc7d099
Zach Rider <zrider99zr@gmail.com>
2017-11-23 00:37:14 -0500

    ggaa

--- api/DBget.php
+++ api/DBget.php
@@ -1,15 +1,16 @@
 <?php
+/*
 require_once(__DIR__ . '/config/global.php');
 function __autoload($className){
   require_once(__DIR__ . '/classes/' . $className . '.php');
 }
 
 $db = Database::getConnection();
-
+*/
 $email = "zrider99zr@gmail.com"
 
 echo $email;
-
+/*
 $qry = $db->prepare("SELECT salt, hash FROM account WHERE emailAddress = ?")
 $qry->bind_param("s",$email);
 $qry->execute();
@@ -17,6 +18,6 @@ $qry->bind_result($dbSalt,$dbHash);
 $qry->store_result();
 
 echo "Salt ", $dbSalt, "<br /> Hash " $dbHash;
-
+*/
 
  ?>
