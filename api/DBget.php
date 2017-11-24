7302c78e94ffd0bcb791cdbc2ae5dad266dee13e
Zach Rider <zrider99zr@gmail.com>
2017-11-23 19:54:08 -0500

    aa

--- api/DBget.php
+++ api/DBget.php
@@ -9,12 +9,13 @@ $db = Database::getConnection();
 
 $email = "zrider99zr@gmail.com";
 
-$sql = "SELECT firstName FROM account";
-$qry = $db->query($sql);
-
+$qry = $db->prepare("SELECT salt, hash FROM account");
+if($qry->execute()){
+  echo "Success";
+}
 if($qry->num_rows >= 1){
   $result = $qry->get_result();
-  echo "Salt ", $result[0]['firstName'];
+  echo "Salt ", $result[0]['salt'], "<br /> Hash ", $result[0]['hash'];
 }
 else{
   echo "<br /> Bad query";
